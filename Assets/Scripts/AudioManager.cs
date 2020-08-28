1c34a48d057c4e8be6a51f918a6c9ab72d598b86
Asier Zunzunegui Valcárcel <asier.zunzu@gmail.com>
2020-08-28 18:45:11 +0200

    More scaffolding

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System;
+using UnityEngine.Audio;
+using UnityEditorInternal;
+
+public class AudioManager : MonoBehaviour
+{
+    public static AudioManager instance;
+    public Sound[] sounds;
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        } else
+        {
+            Destroy(gameObject);
+            return;
+        }
+        DontDestroyOnLoad(gameObject);
+
+        foreach (Sound s in sounds)
+        {
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.clip;
+            s.source.volume = s.volume;
+            s.source.pitch = s.pitch;
+            s.source.loop = s.loop;
+        }
+    }
+
+    void Start()
+    {
+        Play("Theme");
+
+    }
+
+    public void Play(string name)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+        if (s == null)
+        {
+            Debug.LogWarning("Sound  " + name + " does not exist.");
+            return;
+        }
+        Debug.Log("Playing " + name + " clip");
+        s.source.Play();
+    }
+
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public void CloseGame()
+    {
+        Debug.Log("Quitting game");
+        Application.Quit();
+    }
+}
